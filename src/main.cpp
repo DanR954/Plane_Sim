1b0fc84cfe9795cea4a0d6863ce1650fc01de09f
Dan <danielrydon@gmail.com>
2026-01-13 22:30:18 +0000

    fix: changing Hawk to hawk for naming conventions

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,6 @@
 int main() {
 	std::cout << "Starting programme... Initialising plane " << std::endl;
 	std::cout << "Pi value from the physics module:" << physics::pi << std::endl;
-	std::cout << "Hawk plane's useful load:" << Hawk::UsefulLoad << std::endl;
+	std::cout << "Hawk plane's useful load:" << hawk::UsefulLoad << std::endl;
     return 0;
 }
