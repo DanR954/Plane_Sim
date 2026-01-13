70c71866a1d0156ed863d4b67c3462dfca0426ad
Dan <danielrydon@gmail.com>
2026-01-13 22:36:30 +0000

    fix: changing hawk types to be more precise

--- include/hawk.hpp
+++ include/hawk.hpp
@@ -1,9 +1,12 @@
+#include <cstdint>
+
+
 namespace hawk {
-    int CrewCapasity = 2;
-    float Length = 12.43; // Unit in Meters (40 ft 9 in)
-    float Wingspan = 9.94; // Unit in Meters (32 ft 7 in)
-    float Height = 3.98; // Unit in Meters (13 ft 1 in)
-    float WingArea = 16.70; // Unit in M**2 (179.64 ft**2)
-    float EmptyWeight = 4480; // Unit in kg (9,880lb)
-    float UsefulLoad = 3000; //Unit in kg (6600)
+    constexpr uint16_t CrewCapacity = 2;
+    constexpr double Length = 12.43; // Unit in Meters (40 ft 9 in)
+    constexpr double Wingspan = 9.94; // Unit in Meters (32 ft 7 in)
+    constexpr double Height = 3.98; // Unit in Meters (13 ft 1 in)
+    constexpr double WingArea = 16.70; // Unit in M**2 (179.64 ft**2)
+    constexpr double EmptyWeight = 4480; // Unit in kg (9,880lb)
+    constexpr double UsefulLoad = 3000; //Unit in kg (6600)
 };
