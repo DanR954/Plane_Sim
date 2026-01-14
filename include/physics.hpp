2fcfaac04b068d01313929919569fa2b5cd6e781
Dan <danielrydon@gmail.com>
2026-01-14 22:54:14 +0000

    feat: Added Engine and Fuel Tank classes, created file system to create a more modular system than seperates components, templates and universal logic.

--- include/physics.hpp
+++
@@ -1,4 +0,0 @@
-namespace physics {
-	constexpr double pi = 3.14159265358979323846;
-	constexpr double gravity = 9.80665; // m/s^2
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-#include <physics.hpp>
-#include <hawk.hpp>
+#include <universal/physics.hpp>
+#include <templates/hawk.hpp>
+#include <components/engine.hpp>
+#include <universal/maths.hpp>
 
 int main() {
 	std::cout << "Starting programme... Initialising plane " << std::endl;
-	std::cout << "Pi value from the physics module:" << physics::pi << std::endl;
+	std::cout << "Pi value from the physics module:" << maths::pi << std::endl;
 	std::cout << "Hawk plane's useful load:" << hawk::UsefulLoad << std::endl;
+	Engine* enginePtr = new Engine();
+	delete enginePtr; // Testing constructor and destructor
     return 0;
 }
